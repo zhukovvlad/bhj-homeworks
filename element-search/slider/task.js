78db39da28cf6f81d5e7a0752b4502eb68ba7ecd
zhukovvlad <zhukovvlad@yandex.ru>
2019-10-25 13:04:31 +0300

    Final commit
    Everything is working. But without navigations dots.

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,17 +1,8 @@
 let btn = (direction) => {
     let ourBtn = document.getElementsByClassName('slider__arrow_'+direction);
-    console.log(Array.from(ourBtn)[0]);
+    
     let slides = document.getElementsByClassName('slider__item');
-    console.log(slides);
-    let activeElement = Array.from(slides).filter(element => element.classList.contains('slider__item_active'));
-    console.log("Our active element: ", activeElement);
-    let activeSlideIndex = Array.from(slides).indexOf(activeElement[0]);
-    console.log(activeSlideIndex);
-    let counter = {
-        'prev': 0,
-        'next': 0
-    };
-
+    
     function plusOrMinus() {
         if (direction == 'next') {
             return Number(1);
@@ -21,14 +12,18 @@ let btn = (direction) => {
         }
     }
 
-    //function 
-
     ourBtn.item(0).onclick = function() {
-        console.log(plusOrMinus());
-        
-        counter[direction] = (counter[direction]) % (slides.length)+1;
+        let activeElement = Array.from(slides).filter(element => element.classList.contains('slider__item_active'));
+        let activeSlideIndex = Array.from(slides).indexOf(activeElement[0]) + plusOrMinus();
+      
+        if (activeSlideIndex == -1) {
+          activeSlideIndex = slides.length - 1;
+        } else if (activeSlideIndex == slides.length) {
+          activeSlideIndex = 0;
+        }
+      
         Array.from(slides).map(element => element.classList.remove('slider__item_active'));
-        slides[Math.abs(1-counter[direction])].classList.add('slider__item_active');
+        slides[activeSlideIndex].classList.add('slider__item_active');
         console.log(counter);
     }
 }
