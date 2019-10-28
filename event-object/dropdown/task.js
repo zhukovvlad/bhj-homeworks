a0a0787dc11356c51f0012a8740222e01bebe2f8
Vladimir Zhukov <zhukovvlad@yandex.ru>
2019-10-28 22:21:54 +0300

    dropdown menu final commit

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,19 @@
+let dropDown = document.getElementsByClassName('dropdown__list')[0];
+let btn = document.getElementsByClassName('dropdown__value')[0];
+
+let elem = document.getElementsByClassName('dropdown__item');
+
+btn.addEventListener('click', function() {
+  if (dropDown.classList.contains('dropdown__list_active')) {
+    dropDown.classList.remove('dropdown__list_active');
+  } else {
+    dropDown.classList.add('dropdown__list_active');
+  }
+})
+
+for (let index=0; index < elem.length; index++) {
+    elem[index].addEventListener('click', function(event) {
+        btn.textContent = elem[index].textContent;
+        event.preventDefault();
+    });
+}
