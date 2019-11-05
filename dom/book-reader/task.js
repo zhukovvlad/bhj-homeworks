c29b82075ca60283cd73c088feab9f6d41c5299d
Vladimir Zhukov <zhukovvlad@yandex.ru>
2019-11-06 00:06:52 +0300

    Deleted console.logs in js

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,20 +1,14 @@
 const btnList = document.getElementsByClassName('font-size');
-console.log(btnList);
 const book = document.querySelector('div.book');
-console.log(book);
 
 const divColorList = document.querySelector('div.book__control_color');
-console.log(divColorList);
 const btnColorList = divColorList.getElementsByClassName('color');
-console.log(btnColorList);
 
 const divBackList = document.querySelector('div.book__control_background');
 const backgroundList = divBackList.getElementsByClassName('color');
-console.log(backgroundList);
 
 for (let btn of btnList) {
   btn.addEventListener('click', (event) => {
-    console.log(btn.classList);
     Array.from(btnList).map(element => element.classList.remove('font-size_active'));
     btn.classList.add('font-size_active');
     if (btn.classList.contains('font-size_small')) {
@@ -26,14 +20,12 @@ for (let btn of btnList) {
     } else {
       book.classList.remove('book_fs-big', 'book_fs-small');
     }
-    console.log(book.classList);
     event.preventDefault();
   })
 }
 
 for (let btn of btnColorList) {
     btn.addEventListener('click', (event) => {
-        console.log(btn.classList);
         Array.from(btnColorList).map(element => element.classList.remove('color_active'));
         btn.classList.add('color_active');
         if (btn.classList.contains('color_gray')) {
@@ -51,7 +43,6 @@ for (let btn of btnColorList) {
 
 for (let btn of backgroundList) {
     btn.addEventListener('click', (event) => {
-        console.log(btn.classList);
         Array.from(backgroundList).map(element => element.classList.remove('color_active'));
         btn.classList.add('color_active');
         if (btn.classList.contains('color_gray')) {
